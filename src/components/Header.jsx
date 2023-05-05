675212d19f9756dedaa23829f53d4fb06f9dfddd
Mushahid Hussain <mushahid.bscssef20@iba-suk.edu.pk>
2023-05-05 13:22:18 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types'
+
+function Header({ text, bgColor, textColor }) {
+  //{ text } instaed of props then we can just use text in h1
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
+  return (
+    <>
+      <header style={headerStyle}>
+        <div className='container'>
+          <h1>{text}</h1>
+        </div>
+      </header>
+    </>
+  )
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0, 0, 0,0.4)',
+  textColor: '#ff6a95',
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
+
+export default Header
