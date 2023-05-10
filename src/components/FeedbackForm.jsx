560aa458f07eb541df9ad138c855f97f2e3f5985
Mushahid-H <mushahid.bscssef20@iba-suk.edu.pk>
2023-05-10 13:27:03 +0500

    added newFeedback and animation etc.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 import { useState } from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
@@ -20,11 +22,22 @@ function FeedbackForm() {
     }
     setText(e.target.value)
   }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+      handleAdd(newFeedback)
+      setText('')
+    }
+  }
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you like to rate us?</h2>
-        {/* todo rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             type='text'
